ed183d4140d255221e9326e3c620c53e69173743
Alex-taYlor-os <alex.taylor@laplateforme.io>
2026-04-22 22:23:07 +0200

    .

---
+++ Jour01/Job05/script.js
@@ -0,0 +1,16 @@
+function afficherJoursSemaines(week){
+
+    for()
+    
+}
+
+
+let joursSemaines = ["lundi","mardi","mercredi","jeudi","vendredi","samedi","dimanche"];
+
+for (year of listYear){
+    console.log(year)
+    if(bisextile(year))
+        console.log("L'année est bisextile")
+    else    
+         console.log("L'année est commune")
+}
