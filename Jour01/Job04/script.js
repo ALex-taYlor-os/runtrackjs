050ec20a281edecf42cf0ea52070f35ffe7a65ed
Alex-taYlor-os <alex.taylor@laplateforme.io>
2026-04-22 10:34:23 +0200

    .

--- Jour01/Job04/script.js
+++ Jour01/Job04/script.js
@@ -1,3 +1,19 @@
+function bisextile(year){
+
+    if ( (year%4==0 && year%100!=0) | (year%400==0) )
+            return true;
+    else
+            return false;
+    
+}
 
 
+let listYear = [2000,2004, 2008, 2023, 2024];
 
+for (year of listYear){
+    console.log(year)
+    if(bisextile(year))
+        console.log("L'année est bisextile")
+    else    
+         console.log("L'année est commune")
+}
