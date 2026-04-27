61f4d13950774d18434b08ea5340b95c6e6ef49d
Alex-taYlor-os <alex.taylor@laplateforme.io>
2026-04-27 16:54:24 +0200

    .

--- Jour02/Job02/script.js
+++ Jour02/Job02/script.js
@@ -1,12 +1,23 @@
 let bouton = document.querySelector('button');
 
 function showhide(){
-    //Ajouter un article
+    
+    if (document.getElementById("ArticleShowHide") !== null) { //Si mon article existe
+     document.getElementById('ArticleShowHide').remove();
 
-    //Suppression de l'article
-    console.log(document.getElementById("citation").textContent);
+    }else {
+
+        let monarticleShowHide = document.createElement("article");
+
+        monarticleShowHide.textContent="L'important n'est pas la chute, mais l'atterrissage.";
+
+        monarticleShowHide.setAttribute("id","ArticleShowHide");
+
+        document.body.appendChild(monarticleShowHide);
+    }
+    
 };
 
 
-bouton.addEventListener('click', citation); // En mettant citation() avec les parenthèses, tu appelles la fonction tout de suite au lieu de la passer comme callback. Sans les (), tu donnes la fonction à exécuter au moment du clic.
+bouton.addEventListener('click', showhide); // En mettant citation() avec les parenthèses, tu appelles la fonction tout de suite au lieu de la passer comme callback. Sans les (), tu donnes la fonction à exécuter au moment du clic.
 
